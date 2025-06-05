ffb7cbe7a7d94e1153845cb875bf7c8246a407a0
rafal-kozlowski <raffal.kozlowski@gmail.com>
2025-06-05 15:31:39 +0200

    supabase change

--- src/helpers/supabase.ts
+++ src/helpers/supabase.ts
@@ -5,5 +5,26 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const fetchUsers = async() => {
+    const { data, error } = await supabase
+  .from('users')
+  .select()
 
+  if(error){
+    console.log('Error');
+    return [];
+  }
 
+  return data;
+}
+
+export const insertUserToDatabase = async(user:{}) => {
+    const { error } = await supabase
+  .from('countries')
+  .insert(user)
+  
+  if(error){
+    return 'Cannot add user to database';
+  }
+
+}
