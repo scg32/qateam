6e2ac6c5099be7b12f1addb20f7387056b079d42
scg32 <grzes33@gmail.com>
2025-06-17 22:43:59 +0200

    login and register user

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -30,6 +30,7 @@
 </template>
 
 <script setup lang="ts">
+import { loginUser } from '@/helpers/supabase';
 import { ref } from 'vue';
 
 // Definicja reaktywnych zmiennych
@@ -37,9 +38,14 @@ const email = ref('');
 const password = ref('');
 
 // Funkcja logowania
-const login = () => {
+const login = async () => {
   console.log('Logging in with', email.value, password.value);
-  // Możesz tutaj dodać logikę logowania np. z Supabase
+  const { data, error } = await loginUser(email.value, password.value);
+  if (error) {
+    console.error("Login error:", error);
+  } else {
+    console.log("Logged in user:", user);
+  }
 };
 </script>
 
